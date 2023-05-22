7131d9c2859d35dbda351ba3b2d6d614faf8eca2
Lionel <wafulalionel20@gmail.com>
2023-05-22 12:12:47 +0300

    Complete Assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
 
+from distro import name
+
+
 def greet_programmer():
     pass
+print("Hello, programmer!")
+
+#greet_programmer()
 
 def greet(name):
     pass
 
+print(f"Hello,{name}!")
+
 def greet_with_default(name="programmer"):
     pass
+print()
 
 def add(num1, num2):
     pass
+print("num1 + num2")
 
 def halve(number):
     pass
+
+print ("number /2")
